ac3018352505a45e5a4d4bfca751a81f2c5743fd
collective0 <oaklanddealer@gmail.com>
2025-08-09 17:56:17 -0700

    Replace Index.tsx to remove old landing page and preserve chat widget

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,11 @@
-import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import Benefits from "@/components/Benefits";
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Navigation from "@/components/Navigation";
-import DentalShowcase from "@/components/DentalShowcase";
 import FloatingChatbot from "@/components/FloatingChatbot";
 
 const Index = () => {
-  return (
-    <div className="min-h-screen bg-background text-foreground">
-      <Navigation />
-      <DentalShowcase />
-      <Hero />
-      <Services />
-      <Benefits />
-      <About />
-      <Contact />
-      <FloatingChatbot />
-    </div>
-  );
+    return (
+        <div>
+            <FloatingChatbot />
+        </div>
+    );
 };
 
 export default Index;
